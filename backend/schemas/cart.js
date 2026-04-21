c4b61fffb6c21704005e8efe8ff88bc6337dda97
zahidbilal <zahidbilal2k15@gmail.com>
2026-04-21 16:50:34 +0500

    editig the backend end files

--- backend/schemas/cart.js
+++ backend/schemas/cart.js
@@ -1,10 +1,41 @@
-const { productSchema } = require("../schemas/products")
-const mongodb = require("mongoose")
-const cartSchema = new mongodb.Schema({
-    name: {
-        type: String,
-        required: true
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [{
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
     },
-    cartItem: [productSchema]
-})
-module.exports = mongodb.model("cart", cartSchema)
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1
+    },
+    price: {
+      type: Number,
+      required: true
+    }
+  }],
+  totalAmount: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
+
